146a373c8a3a563c48d0f91b450a654f62a54af8
yishaqi <2543671857@qq.com>
2021-01-22 17:42:08 +0800

    first
    Signed-off-by: yishaqi <2543671857@qq.com>

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mylibrary.People;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String name = new People().getName();
+    }
+}
